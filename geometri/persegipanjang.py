9377d98b3780007d4bda479cc550079d221e57f3
spbudi <accggold20@gmail.com>
2021-06-08 20:11:15 +0700

    OOP

---
+++ geometri/persegipanjang.py
@@ -0,0 +1,11 @@
+class PersegiPanjang():
+    def __init__(self, p, l):
+        #fungsi yang dipanggil pertama kali saat objek diciptakan
+        self.p = p
+        self.l = l
+
+    def info(self):
+        return f'Ini adalah objek Persegi panjang dengan panjang = {self.p} dan lebar = {self.l} memiliki luas'
+
+    def hitung_luas(self):
+        return self.p * self.l
